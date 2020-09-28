ecbd084142c8c75a2bdad29594a7073666079054
JayaWikrama <jayawikrama89@gmail.com>
2020-09-28 13:06:32 +0700

    Merubah skema debug agar lebih hemat buffer text memmory

--- shiki-linked-list.c
+++ shiki-linked-list.c
@@ -20,57 +20,61 @@
 
 int8_t debug_mode_status = 1;
 
-static void shilink_debug(const char *function_name, char *debug_type, char *debug_msg, ...){
-	if (debug_mode_status == 1 || strcmp(debug_type, "INFO") != 0){
-        struct tm *d_tm;
+typedef enum {
+  SHILINK_DEBUG_INFO = 0x00,
+  SHILINK_DEBUG_VERSION = 0x01,
+  SHILINK_DEBUG_WARNING = 0x02,
+  SHILINK_DEBUG_ERROR = 0x03,
+  SHILINK_DEBUG_CRITICAL = 0x04
+} shilink_debug_type;
+
+static void shilink_debug(const char *_function_name, shilink_debug_type _debug_type, char *_debug_msg, ...){
+	if (debug_mode_status == 1 || _debug_type != SHILINK_DEBUG_INFO){
+        struct tm *d_tm = NULL;
         struct timeval tm_debug;
         uint16_t msec = 0;
-	    va_list aptr;
 		
 	    gettimeofday(&tm_debug, NULL);
 	    d_tm = localtime(&tm_debug.tv_sec);
         msec = tm_debug.tv_usec/1000;
-	
-	    char* tmp_debug_msg;
-        tmp_debug_msg = (char *) malloc(256*sizeof(char));
-        if (tmp_debug_msg == NULL){
-            printf("%02d-%02d-%04d %02d:%02d:%02d.%03i ERROR: %s: failed to allocate debug variable memory",
-             d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec, msec, __func__
-            );
-            return;
-        }
-	    va_start(aptr, debug_msg);
-	    vsprintf(tmp_debug_msg, debug_msg, aptr);
-	    va_end(aptr);
+
         #ifdef __linux__
-            if (strcmp(debug_type, "INFO")==0)
-                printf("\033[1;32m%02d-%02d-%04d %02d:%02d:%02d.%03d\033[1;34m SHILINK\033[1;32m %s: %s: %s\033[0m",
+            if (_debug_type == SHILINK_DEBUG_INFO)
+                printf("%02d-%02d-%04d %02d:%02d:%02d.%03d\033[0;34m SHILINK\033[1;32m INFO\033[0m %s: ",
                  d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec,
-                 msec, debug_type, function_name, tmp_debug_msg
+                 msec, _function_name
                 );
-    	    else if (strcmp(debug_type, "WARNING")==0)
-                printf("\033[1;33m%02d-%02d-%04d %02d:%02d:%02d.%03d\033[1;34m SHILINK\033[1;33m %s: %s: %s\033[0m",
+            else if (_debug_type == SHILINK_DEBUG_VERSION)
+                printf("%02d-%02d-%04d %02d:%02d:%02d.%03d\033[0;34m SHILINK\033[1;32m VERSION\033[0m %s: ",
                  d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec,
-                 msec, debug_type, function_name, tmp_debug_msg
+                 msec, _function_name
                 );
-    	    else if (strcmp(debug_type, "ERROR")==0)
-                printf("\033[1;31m%02d-%02d-%04d %02d:%02d:%02d.%03d\033[1;34m SHILINK\033[1;31m %s: %s: %s\033[0m",
+    	    else if (_debug_type == SHILINK_DEBUG_WARNING)
+                printf("%02d-%02d-%04d %02d:%02d:%02d.%03d\033[0;34m SHILINK\033[1;33m WARNING\033[0m %s: ",
                  d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec,
-                 msec, debug_type, function_name, tmp_debug_msg
+                 msec, _function_name
                 );
-            else if (strcmp(debug_type, "CRITICAL")==0)
-                printf("\033[1;31m%02d-%02d-%04d %02d:%02d:%02d.%03d\033[1;34m SHILINK\033[1;31m %s: %s: %s\033[0m",
+    	    else if (_debug_type == SHILINK_DEBUG_ERROR)
+                printf("%02d-%02d-%04d %02d:%02d:%02d.%03d\033[0;34m SHILINK\033[1;31m ERROR\033[0m %s: ",
                  d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec,
-                 msec, debug_type, function_name, tmp_debug_msg
+                 msec, _function_name
+                );
+            else if (_debug_type == SHILINK_DEBUG_CRITICAL)
+                printf("%02d-%02d-%04d %02d:%02d:%02d.%03d\033[0;34m SHILINK\033[1;31m CRITICAL\033[0m %s: ",
+                 d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec,
+                 msec, _function_name
                 );
 	    #else
-            printf("%02d-%02d-%04d %02d:%02d:%02d.%03d SCONF %s: %s: %s",
+            printf("%02d-%02d-%04d %02d:%02d:%02d.%03d [%02x]: %s: ",
              d_tm->tm_mday, d_tm->tm_mon+1, d_tm->tm_year+1900, d_tm->tm_hour, d_tm->tm_min, d_tm->tm_sec,
-             msec, debug_type, function_name, tmp_debug_msg
+             msec, _debug_type, _function_name
             );
         #endif
-        free(tmp_debug_msg);
-        tmp_debug_msg = NULL;
+
+        va_list aptr;
+        va_start(aptr, _debug_msg);
+	    vfprintf(stdout, _debug_msg, aptr);
+	    va_end(aptr);
     }
 }
 
@@ -97,7 +101,7 @@ long shilink_get_version(char *_version){
 }
 
 void shilink_view_version(){
-    shilink_debug(__func__, "VERSION", "%s\n", SHILINK_VER);
+    shilink_debug(__func__, SHILINK_DEBUG_VERSION, "%s\n", SHILINK_VER);
 }
 
 /* USER MODIFICATION PURPOSE START HERE */
@@ -183,14 +187,14 @@ int8_t shilink_fill_custom_data(
     if (_key != NULL){
         _data->sl_key = (char *) malloc(_sizeof_key + 1);
         if (_data->sl_key == NULL){
-            shilink_debug(__func__, "ERROR", "failed to allocate memory. process aborted!\n");
+            shilink_debug(__func__, SHILINK_DEBUG_ERROR, "failed to allocate memory. process aborted!\n");
             return -1;
         }
     }
     if (_value != NULL){
         _data->sl_value = (char *) malloc(_sizeof_value + 1);
         if (_data->sl_value == NULL){
-            shilink_debug(__func__, "ERROR", "failed to allocate memory. process aborted!\n");
+            shilink_debug(__func__, SHILINK_DEBUG_ERROR, "failed to allocate memory. process aborted!\n");
             if (_key != NULL){
                 free(_data->sl_key);
                 _data->sl_key = NULL;
@@ -335,7 +339,7 @@ int8_t shilink_search_data_by_prev_cond(
 ){
     if (_target == NULL){
         shilink_free_custom_data(_prev_cond_data);
-        shilink_debug(__func__, "ERROR", "_target is NULL pointer\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_target is NULL pointer\n");
         return -1;
     }
     while (_target != NULL){
@@ -354,7 +358,7 @@ int8_t shilink_search_data_by_prev_cond(
         }
         shilink_free_custom_data(_prev_cond_data);
         if (_target == NULL){
-            shilink_debug(__func__, "ERROR", "condition not found\n");
+            shilink_debug(__func__, SHILINK_DEBUG_ERROR, "condition not found\n");
             return -1;
             break;
         }
@@ -371,7 +375,7 @@ int8_t shilink_search_data_by_prev_cond(
             _target = _target->sh_next;
         }
     }
-    shilink_debug(__func__, "WARNING", "data not found\n");
+    shilink_debug(__func__, SHILINK_DEBUG_WARNING, "data not found\n");
     return -1;
 }
 
@@ -386,7 +390,7 @@ int8_t shilink_search_data_by_pos_and_prev_cond(
     int16_t idx_pos = -1;
     if (_target == NULL){
         shilink_free_custom_data(_prev_cond_data);
-        shilink_debug(__func__, "ERROR", "_target is NULL pointer\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_target is NULL pointer\n");
         return -1;
     }
     while (_target != NULL){
@@ -408,11 +412,11 @@ int8_t shilink_search_data_by_pos_and_prev_cond(
         }
         shilink_free_custom_data(_prev_cond_data);
         if (_target == NULL){
-            shilink_debug(__func__, "ERROR", "condition not found\n");
+            shilink_debug(__func__, SHILINK_DEBUG_ERROR, "condition not found\n");
             return -1;
         }
         else if (_pos != idx_pos){
-            shilink_debug(__func__, "ERROR", "position not found\n");
+            shilink_debug(__func__, SHILINK_DEBUG_ERROR, "position not found\n");
             return -2;
         }
         while (_target != NULL){
@@ -428,7 +432,7 @@ int8_t shilink_search_data_by_pos_and_prev_cond(
             _target = _target->sh_next;
         }
     }
-    shilink_debug(__func__, "WARNING", "data not found\n");
+    shilink_debug(__func__, SHILINK_DEBUG_WARNING, "data not found\n");
     return -1;
 }
 /* USER MODIFICATION PURPOSE END HERE */
@@ -437,7 +441,7 @@ int8_t shilink_push(SHLink *_target, SHLinkCustomData _data){
     SHLink new_data = NULL;
     new_data = (SHLink) malloc(sizeof(struct shilink_var));
     if (new_data == NULL){
-        shilink_debug(__func__, "ERROR", "failed to allocate memory\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "failed to allocate memory\n");
         return -1;
     }
     shilink_fill_data(&new_data, _data);
@@ -450,7 +454,7 @@ int8_t shilink_append(SHLink *_target, SHLinkCustomData _data){
     SHLink new_data = NULL;
     new_data = (SHLink) malloc(sizeof(struct shilink_var));
     if (new_data == NULL){
-        shilink_debug(__func__, "ERROR", "failed to allocate memory\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "failed to allocate memory\n");
         return -1;
     }
     shilink_fill_data(&new_data, _data);
@@ -473,12 +477,12 @@ int8_t shilink_append(SHLink *_target, SHLinkCustomData _data){
 
 static int8_t shilink_insert(SHLink *_target, SHLinkCustomData _data_cond, SHLinkCustomData _data, int8_t _mode){
     if (shilink_check_custom_data(_data) != 0){
-        shilink_debug(__func__, "ERROR", "_data_new is not set. process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_data_new is not set. process aborted\n");
         return -1;
     }
 
     if (*_target == NULL){
-        shilink_debug(__func__, "ERROR", "_target (null). process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_target (null). process aborted\n");
         return -1;
     }
 
@@ -496,7 +500,7 @@ static int8_t shilink_insert(SHLink *_target, SHLinkCustomData _data_cond, SHLin
     }
 
     if (tmp == NULL){
-        shilink_debug(__func__, "WARNING", "cond_data not found. process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_WARNING, "cond_data not found. process aborted\n");
         return -2;
     }
     
@@ -508,7 +512,7 @@ static int8_t shilink_insert(SHLink *_target, SHLinkCustomData _data_cond, SHLin
     SHLink new_data = NULL;
     new_data = (SHLink) malloc(sizeof(struct shilink_var));
     if (new_data == NULL){
-        shilink_debug(__func__, "ERROR", "failed to allocate memory\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "failed to allocate memory\n");
         return -1;
     }
     shilink_fill_data(&new_data, _data);
@@ -529,12 +533,12 @@ int8_t shilink_insert_before(SHLink *_target, SHLinkCustomData _data_cond, SHLin
 
 int8_t shilink_delete(SHLink *_target, SHLinkCustomData _data){
     if (shilink_check_custom_data(_data) != 0){
-        shilink_debug(__func__, "ERROR", "_data is not set. process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_data is not set. process aborted\n");
         return -1;
     }
 
     if (*_target == NULL){
-        shilink_debug(__func__, "ERROR", "_target (null). process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_target (null). process aborted\n");
         return -1;
     }
 
@@ -567,17 +571,17 @@ int8_t shilink_delete(SHLink *_target, SHLinkCustomData _data){
 
 int8_t shilink_update(SHLink *_target, SHLinkCustomData _data_old, SHLinkCustomData _data_new){
     if (shilink_check_custom_data(_data_old) != 0){
-        shilink_debug(__func__, "ERROR", "_data_old is not set. process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_data_old is not set. process aborted\n");
         return -1;
     }
 
     if (shilink_check_custom_data(_data_new) != 0){
-        shilink_debug(__func__, "ERROR", "_data_new is not set. process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_data_new is not set. process aborted\n");
         return -1;
     }
 
     if (*_target == NULL){
-        shilink_debug(__func__, "ERROR", "_target (null). process aborted\n");
+        shilink_debug(__func__, SHILINK_DEBUG_ERROR, "_target (null). process aborted\n");
         return -1;
     }
 
